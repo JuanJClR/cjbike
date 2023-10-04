3862ea6c7494192daf44a8e1c6c84c528c8fa00e
Mkclaren <mkclaren.tobar@upb.edu.co>
2023-10-04 11:55:40 -0500

    Agregar cambios Generales
    Se agrego el index, se corrigieron errores y ademas se agregaron los assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importa los componentes que deseas usar en las rutas
+import { HomeComponent } from './home/home.component'; // Asegúrate de tener el componente "Home" importado
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta para la página principal (puedes cambiarla a tu componente principal)
+  // Agrega más rutas según tus necesidades
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,7 @@
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-// Librerías 
-import 'jquery';
-import 'popper.js'; 
-import 'bootstrap';
-import 'owl.carousel';
-import '@fortawesome/fontawesome-free';
-import 'animate.css';
-import 'magnific-popup';
-
-// Types
-import '@types/jquery';
-import '@types/popper.js';
-import '@types/bootstrap'; 
-import '@types/fontawesome';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
