eaa7f19970dde2d156c1502ef339c902e72a130d
axensz <camiloguzman2703@gmail.com>
2023-10-04 22:20:09 -0500

    Configuracion General

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: [
+    './about.component.css',
+    '../../assets/css/main.css'
+]
 })
 export class AboutComponent {
 
