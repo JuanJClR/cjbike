349d97e1565c83d0aa635adb7df123c90a9dea62
JuanJose <juanj.calderon@upb.edu.co>
2023-10-06 00:11:44 -0500

    Se agregó funcion login

--- cjbike-front/src/app/login/login.component.ts
+++ cjbike-front/src/app/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component, AfterViewInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    const data = {email: this.email, password: this.password };
+
+    axios.post('http://localhost:3000/login', data)
+      .then(response => {
+        console.log('Respuesta del servidor:', response.data);
+      })
+      .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+      });
+  }
 }
+
