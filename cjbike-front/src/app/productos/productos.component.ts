6609faf925f1af7a09335ae3bcd257f4a19a4110
JuanJose <juanj.calderon@upb.edu.co>
2023-10-06 03:18:16 -0500

    Agregado productos

--- cjbike-front/src/app/productos/productos.component.ts
+++ cjbike-front/src/app/productos/productos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
 
+export class ProductosComponent implements OnInit {
+
+  productos: any[] = [];
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    axios.get('http://localhost:3000/productos')
+      .then(response => {
+        this.productos = response.data;
+      });
+  }
 }
