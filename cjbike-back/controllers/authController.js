9cff3d988c78a5fa99f55264b087d787edd9053d
JuanJose <juanj.calderon@upb.edu.co>
2023-10-05 23:41:07 -0500

    Se agregó función que almacena los usuarios en la db

--- cjbike-back/controllers/authController.js
+++ cjbike-back/controllers/authController.js
@@ -3,22 +3,31 @@ const db = admin.firestore();
 const auth = admin.auth();
 
 function registerUser(req, res) {
-  const { email, password } = req.body;
+  const { name, email, password } = req.body;
 
   admin.auth().createUser({
     email: email,
     password: password,
+    displayName: name // Añade el nombre como displayName
   })
   .then((userRecord) => {
-    // El usuario ha sido creado exitosamente
-    res.send(`Usuario registrado: ${userRecord.uid}`);
+    // Usuario creado exitosamente
+    // Ahora, guardamos el nombre en Firestore
+    db.collection('usuarios').doc(userRecord.uid).set({
+      nombre: name,
+      email: email
+    })
+    .then(() => {
+      res.send(`Usuario registrado: ${userRecord.uid}`);
+    })
+    .catch((error) => {
+      res.status(500).send(`Error al guardar el nombre en Firestore: ${error.message}`);
+    });
   })
   .catch((error) => {
-    // Hubo un error al crear el usuario
     res.status(500).send(`Error al registrar usuario: ${error.message}`);
   });
 }
-
 
 module.exports = {
   registerUser,
