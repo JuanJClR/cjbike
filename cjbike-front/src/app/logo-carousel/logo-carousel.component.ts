b080c819e4969db5ef51789e7b0802b930f3ca47
axensz <camiloguzman2703@gmail.com>
2023-10-06 00:21:55 -0500

    Arreglo logo-carousel.component.ts

--- cjbike-front/src/app/logo-carousel/logo-carousel.component.ts
+++ cjbike-front/src/app/logo-carousel/logo-carousel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-logo-carousel',
   templateUrl: './logo-carousel.component.html',
-  styleUrls: ['./logo-carousel.component.css', '../../assets/css/carousel.css']
+  styleUrls: ['./logo-carousel.component.css', '../../assets/css/main.css']
 })
 export class LogoCarouselComponent {
 
