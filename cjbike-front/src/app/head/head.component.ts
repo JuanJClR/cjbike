88766eed831a9ce73f805dcfd333689cba34bbfa
axensz <camiloguzman2703@gmail.com>
2023-11-01 06:46:36 -0500

    Cambios generales

--- cjbike-front/src/app/head/head.component.ts
+++ cjbike-front/src/app/head/head.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
-  styleUrls: [
-    './head.component.css',
-    '../../assets/css/main.css'
-]
+  styleUrls: ['./head.component.css']
 })
-export class HeadComponent {
+export class HeadComponent implements OnInit {
+  user: User | null = null;
 
+  constructor(private afAuth: AngularFireAuth) { }
+
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
 }
